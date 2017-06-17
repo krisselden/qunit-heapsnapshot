cdc990e348aedbcd3f22f3174a8459d16cc7ffa6
Kris Selden <kris.selden@gmail.com>
2017-06-16 20:54:37 -0700

    some fixes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const url = require('url');
 const Heapsnapshot = require('heapsnapshot');
 const index = require('chrome-debugging-client');
 const protocol = require('chrome-debugging-client/dist/protocol/tot');
@@ -14,7 +15,6 @@ runQUnitTestsWithSnapshots(process.argv[2]).catch((err) => {
 function runQUnitTestsWithSnapshots(url) {
   return index.createSession(async (session) => {
     const browser = await session.spawnBrowser('canary', {
-      additionalArguments: ['--headless'],
     });
 
     const apiClient = session.createAPIClient('localhost', browser.remoteDebuggingPort);
@@ -26,6 +26,11 @@ function runQUnitTestsWithSnapshots(url) {
     const runtime = new protocol.Runtime(client);
     const heapProfiler = new protocol.HeapProfiler(client);
     const debug = new protocol.Debugger(client);
+    const cons = new protocol.Console(client);
+
+    cons.messageAdded = (evt) => {
+      console.log(evt.message.level, evt.message.text);
+    };
 
     const scriptMap = new Map();
 
@@ -37,55 +42,73 @@ function runQUnitTestsWithSnapshots(url) {
       scriptMap.set(evt.scriptId, evt);
     }
 
-    await Promise.all([page.enable(), debug.enable(), runtime.enable(), heapProfiler.enable()]);
-
-    await page.addScriptToEvaluateOnLoad({
-      scriptSource: `
-        QUnit.testDone(() => {
-          QUnit.stop();
-          __testDone(false);
-        });
-        QUnit.done(() => {
-          __testDone(true);
-        });
-        function resumeTest() {
-          QUnit.start();
-          return new Promise(resolve => window.__testDone = resolve);
-        }
-      `
-    });
-
-    let contextId;
+    await Promise.all([page.enable(), debug.enable(), runtime.enable(), heapProfiler.enable(), cons.enable()]);
 
     const contextCreated = eventPromise(runtime, 'executionContextCreated').then((evt) => {
-      contextId = evt.id;
+      const contextId = evt.context.id;
       return runtime.evaluate({
         contextId,
         expression: `window.QUnit = { config: { autostart: false } };`,
         returnByValue: true
-      });
+      }).then(() => contextId);
     });
 
     const pageLoad = eventPromise(page, 'loadEventFired');
 
     await page.navigate({ url });
 
-    await Promise.all([ pageLoad, contextCreated ]);
+    const contextId = await contextCreated;
+
+    await pageLoad;
+
+    const scriptId = await compileScript(runtime, `
+let __testStarted = false;
+let __testDone = undefined;
+
+QUnit.on("runStart", function () {
+  __testStarted = true;
+  console.debug('>>>>  RUN START');
+});
+
+QUnit.on("testEnd", function () {
+  console.debug('>>>>  TEST END');
+  QUnit.config.blocking = true;
+  __testDone(false);
+});
+
+QUnit.on("runEnd", function () {
+  console.debug('>>>>  RUN END');
+  __testDone(true);
+});
+
+function __resumeTest() {
+  if (!__testStarted) {
+    console.debug('>>>>  QUnit.start');
+    QUnit.start();
+  } else {
+    QUnit.config.blocking = false;
+  }
+
+  return new Promise(function(resolve) {
+    __testDone = resolve;
+  });
+}
+`, 'http://localhost:7357/__pageload.js', contextId);
+    let loadResult = await runtime.runScript({
+      scriptId: scriptId
+    });
 
     while (true) {
       let result = await runtime.evaluate({
         contextId,
-        expression: `resumeTest()`,
+        expression: `__resumeTest()`,
         awaitPromise: true
       });
-      if (result.exceptionDetails) {
-        throw new Error(JSON.stringify(result.exceptionDetails));
-      }
 
       let isDone = toBoolean(result);
       if (isDone) break;
 
-      snapshot = await takeHeapSnapshot();
+      snapshot = await takeHeapSnapshot(heapProfiler);
       snapshot.buildSync();
 
       for (const node of snapshot) {
@@ -134,4 +157,17 @@ function eventPromise(domain, eventName) {
       resolve(evt);
     }
   });
+}
+
+async function compileScript(runtime, expression, sourceURL, executionContextId) {
+  const result = await runtime.compileScript({
+    expression,
+    sourceURL,
+    persistScript: true,
+    executionContextId
+  });
+  if (result.exceptionDetails) {
+    throw new Error(JSON.stringify(result.exceptionDetails));
+  }
+  return result.scriptId;
 }
